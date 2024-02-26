9ecd10e853c7cd3fde050cd2cafe9ac7cd086a66
João Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2024-02-26 12:50:50 -0300

    feat: base funcional do clone

---
+++ angular-buzzfeed-clone/src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css']
+})
+export class QuizzComponent implements OnInit {
+  title: string = "teste"
+  questions: any;
+  questionsSelected: any;
+  answers: string[] = [];
+  answerSelected: string ="";
+  questionIndex: number = 0;
+  questionMax: number = 0;
+  finished: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false;
+      this.title = quizz_questions.title;
+      this.questions = quizz_questions.questions;
+      this.questionsSelected = this.questions[this.questionIndex];
+      this.questionIndex = 0;
+      this.questionMax = this.questions.length;
+    }
+  }
+
+  playerChoose(value: string){
+    this.answers.push(value);
+    this.nextStep()
+  }
+
+  async nextStep(){
+    this.questionIndex += 1;
+    if(this.questionMax > this.questionIndex){
+      this.questionsSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
+    }
+  }
+
+  async checkResult(answers: string[]){
+    const result = answers.reduce((previous, current, i, arr)=>{
+        if(arr.filter(item => item === previous).length > arr.filter(item => item === current).length ){
+            return previous;
+        }else{
+          return current;
+        }
+    })
+    return result;
+  }
+
+}
